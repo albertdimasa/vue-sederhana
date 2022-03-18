6a8bff72fc8df1797933132deec5a8b3c7665700
albertdimasa <albertabraham28@gmail.com>
2022-03-18 16:00:34 +0700

    Membuat Task Sederhana

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ol>
+      <li v-for="todo in listTodo" :key="todo.id">
+        {{ todo }}
+      </li>
+    </ol>
+    <input type="text" v-model="todo" />
+    <button @click="tambahkan(todo)">Tambahkan</button>
+    <text-hebat :list-todo="listTodo.length >= 4"></text-hebat>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TextHebat from "@/components/TextHebat.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TextHebat,
+  },
+  data() {
+    return {
+      todo: "",
+      listTodo: [],
+    };
+  },
+  methods: {
+    tambahkan(todo) {
+      this.listTodo.push(todo);
+      this.todo = "";
+    },
+  },
+};
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+#app li {
+  text-transform: capitalize;
 }
 </style>
---
+++ src/components/TextHebat.vue
@@ -0,0 +1,12 @@
+<template>
+  <p v-if="listTodo">Hebat!</p>
+</template>
+
+<script>
+export default {
+  name: "TextHebat",
+  props: ["listTodo"],
+};
+</script>
+
+<style scoped></style>
